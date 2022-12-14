572728e0f8c82c9c96fd067149d630ae8fbb0fa4
Samuel Coizet <messa57fr@gmail.com>
2022-12-14 09:57:34 +0100

    update project

---
+++ src/components/Projects/index.js
@@ -0,0 +1,121 @@
+import React, { useEffect, useState } from "react";
+import Loader from "react-loaders";
+import AnimatedLetters from "../AnimatedLetters";
+import "./index.scss";
+import Pizza from "../../assets/images/pizza-next.png";
+import {
+  Grid,
+  Card,
+  CardActionArea,
+  Typography,
+  CardMedia,
+  CardContent,
+} from "@mui/material";
+import { Linking } from "react";
+
+const Portfolio = () => {
+  const [letterClass, setLetterClass] = useState("text-animate");
+
+  useEffect(() => {
+    setTimeout(() => {
+      return setLetterClass("text-animate-hover");
+    }, 2500);
+  }, []);
+
+  return (
+    <>
+      <div className="container projects-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              strArray={["P", "r", "o", "j", "e", "c", "t", "s"]}
+              idx={15}
+              letterClass={letterClass}
+            />
+          </h1>
+          <p>Here is some of my projects.</p>
+          <p>
+            Some have been done during my bootcamp at Le Wagon while others are
+            just personnal training.
+          </p>
+          <Grid container spacing={2}>
+            <Grid xs={12} sm={6} md={4}>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardActionArea
+                  component="a"
+                  href="http://pizza-vert-six.vercel.app"
+                  target="_blank"
+                >
+                  <CardMedia
+                    component="img"
+                    height="140"
+                    image={Pizza}
+                    alt="pizza"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h3" component="div">
+                      Lizard
+                    </Typography>
+                    <Typography variant="body">
+                      Lizards are a widespread group of squamate reptiles, with
+                      over 6,000 species, ranging across all continents except
+                      Antarctica
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+            <Grid xs={12} sm={6} md={4}>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="140"
+                    image={Pizza}
+                    alt="green iguana"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Lizard
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Lizards are a widespread group of squamate reptiles, with
+                      over 6,000 species, ranging across all continents except
+                      Antarctica
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+            <Grid xs={12} sm={6} md={4}>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="140"
+                    image={Pizza}
+                    alt="green iguana"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Lizard
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Lizards are a widespread group of squamate reptiles, with
+                      over 6,000 species, ranging across all continents except
+                      Antarctica
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          </Grid>
+        </div>
+      </div>
+
+      <Loader type="pacman" />
+    </>
+  );
+};
+
+export default Portfolio;
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,49 +1,57 @@
-import './index.scss';
-import Logo from '../../assets/images/logo.jpg'
-import { Link, NavLink } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import "./index.scss";
+import Logo from "../../assets/images/logo.jpg";
+import { Link, NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEnvelope,
+  faFile,
+  faHome,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const Sidebar = () => (
-  <div className='nav-bar'>
-    <Link className='logo' to='/'>
+  <div className="nav-bar">
+    <Link className="logo" to="/">
       <img src={Logo} alt="logo" />
       <span>Samuel</span>
       <span>Coizet</span>
     </Link>
     <nav>
-      <NavLink exact = "true" activeclassname="active" to="/">
-        <FontAwesomeIcon icon = {faHome} color="#4d4d4e"/>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
       </NavLink>
-      <NavLink exact = "true" activeclassname="about-link" to="/about">
-        <FontAwesomeIcon icon = {faUser} color="#4d4d4e"/>
+      <NavLink exact="true" activeclassname="about-link" to="/about">
+        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
       </NavLink>
-      <NavLink exact = "true" activeclassname="contact-link" to="/contact">
-        <FontAwesomeIcon icon = {faEnvelope} color="#4d4d4e"/>
+      <NavLink exact="true" activeclassname="contact-link" to="/contact">
+        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+      </NavLink>
+      <NavLink exact="true" activeclassname="projects" to="/projects">
+        <FontAwesomeIcon icon={faFile} color="#4d4d4e" />
       </NavLink>
     </nav>
     <ul>
       <li>
         <a
-        target ="_blank"
-        href="https://www.linkedin.com/in/samuel-coizet/"
-        rel="noreferrer"
+          target="_blank"
+          href="https://www.linkedin.com/in/samuel-coizet/"
+          rel="noreferrer"
         >
-        <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
         </a>
       </li>
       <li>
         <a
-        target ="_blank"
-        href="https://www.github.com/messa57fr"
-        rel="noreferrer"
+          target="_blank"
+          href="https://www.github.com/messa57fr"
+          rel="noreferrer"
         >
-        <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
         </a>
       </li>
     </ul>
   </div>
-)
+);
 
-export default Sidebar
+export default Sidebar;
