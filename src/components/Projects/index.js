423025dcd9d2b0347bdcb9bb6004fe7c5f064031
Samuel Coizet <messa57fr@gmail.com>
2022-12-14 10:02:56 +0100

    update project

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -11,7 +11,6 @@ import {
   CardMedia,
   CardContent,
 } from "@mui/material";
-import { Linking } from "react";
 
 const Portfolio = () => {
   const [letterClass, setLetterClass] = useState("text-animate");
